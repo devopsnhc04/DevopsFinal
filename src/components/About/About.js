6beae133889824aa3810d12b193970e6ccb55d0a
devopsnhc04 <126224058+devopsnhc04@users.noreply.github.com>
2023-03-02 10:17:37 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,7 +14,7 @@ function About() {
       <div className="about-nike-part">
         <h2 className="about-nike-heading">About Nike</h2>
         <p className='about-nike-para'>
-          Our missioddn is what drives us to do everything possible to expand
+          Our mission is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreadddking sport
           innovations, by making our products more sudddstainably, by building a
           creative and diverseacadcadc global team and by making a positive impact in
